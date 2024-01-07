3b6acc2d59eea4d6c70dc68a1455f5786a1c4acd
Srikanth Munaga <srikanthsiri.ms@gmail.com>
2024-01-07 03:32:02 -0600

    added lambda handler code

--- src/main/java/com/example/BuildTest/BuildTestApplication.java
+++ src/main/java/com/example/BuildTest/BuildTestApplication.java
@@ -1,13 +1,17 @@
 package com.example.BuildTest;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
 
 @SpringBootApplication
-public class BuildTestApplication {
+public class BuildTestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuildTestApplication.class, args);
 	}
+	
+	
 
 }
